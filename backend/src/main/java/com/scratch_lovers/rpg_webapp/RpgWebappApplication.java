785b4ecfa7b8cf5dd3114d8931a63e0b14858205
Mendrejk <bastian.lakomy@gmail.com>
2021-06-24 00:37:22 +0200

    Initialise project from Spring Initializr

---
+++ backend/src/main/java/com/scratch_lovers/rpg_webapp/RpgWebappApplication.java
@@ -0,0 +1,13 @@
+package com.scratch_lovers.rpg_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgWebappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/scratch_lovers/rpg_webapp/RpgWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scratch_lovers.rpg_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
